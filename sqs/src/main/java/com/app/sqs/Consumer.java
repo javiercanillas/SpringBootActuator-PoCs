3cb6926e76d25bc027cb66ebbe502236002be583
Javier Canillas <javier.canillas@recargapay.com>
2020-11-27 19:00:07 -0300

    Logging message headers

--- sqs/src/main/java/com/app/sqs/Consumer.java
+++ sqs/src/main/java/com/app/sqs/Consumer.java
@@ -4,8 +4,11 @@ import com.app.model.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
+import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 @Component
 public class Consumer {
@@ -13,7 +16,7 @@ public class Consumer {
     private static final Logger LOG = LoggerFactory.getLogger(Consumer.class);
 
     @SqsListener("${sqsName}")
-    public void receiveMessage(@Payload Message message) {
-        LOG.info("Consumed: {}", message);
+    public void receiveMessage(@Payload Message message, @Headers Map<String,Object> headers) {
+        LOG.info("Consumed: {} with headers {}", message, headers);
     }
 }
